9c83311ab03cefe5534afbe0de1da6facb1fd515
Karnainali <karnainali39@gmail.com>
2023-08-14 16:31:28 +0500

    read me file

---
+++ app.js
@@ -0,0 +1,25 @@
+
+(function(){
+'use strict';
+angular.module('DIApp', [])
+
+.controller('DIController', DIController);
+
+DIController.inject = ['$scope', '$filter'];
+  function DIController($scope, $filter) {
+    $scope.name = "Yaakov";
+  
+
+  $scope.upper = function () {
+    let upCase = $filter('uppercase');
+    $scope.name = upCase($scope.name);
+  };
+
+}
+
+
+
+})();
+
+
+
