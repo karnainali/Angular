b41961268b4c447a07f1f057561bea7a4e0fb146
Karnainali <karnainali39@gmail.com>
2023-08-14 22:26:14 +0500

    read me file

---
+++ mod1_solution/App.js
@@ -0,0 +1,35 @@
+(function() {
+    'use strict';
+  
+    angular.module('LunchChecker', [])
+  
+    .controller('CheckerController', CheckerController);
+  
+    CheckerController.$inject = ['$scope'];
+  
+    function CheckerController($scope) {
+      $scope.inputItems = '';
+      $scope.showMessage = false;
+      $scope.message = '';
+  
+      $scope.checkItems = function() {
+        if ($scope.inputItems === '') {
+          $scope.message = 'Enter Data please';
+        } else {
+          const items = $scope.inputItems.split(',').map(item => item.trim());
+  
+          // Check for empty or invalid items
+          if (items.some(item => item === '' || /[!@#$%^&*()_+{}\[\]:;<>,.?~\\/`\-=]/.test(item))) {
+            $scope.message = 'Note: Do not put comma at the end of item also No empty or invalid items';
+          } else if (items.length <= 3) {
+            $scope.message = 'ENJOY';
+          } else {
+            $scope.message = 'TOO MUCH';
+          }
+        }
+        $scope.showMessage = true;
+      }
+    }
+  
+  })();
+  
